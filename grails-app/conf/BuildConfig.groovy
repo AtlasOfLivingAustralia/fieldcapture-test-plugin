8b316b4aa1e685e317f495b8a99770b45356c4c0
chrisala <chris.godwin.ala@gmail.com>
2015-06-12 12:26:30 +1000

    Created a plugin for common test code.

---
+++ grails-app/conf/BuildConfig.groovy
@@ -0,0 +1,52 @@
+grails.project.class.dir = "target/classes"
+grails.project.test.class.dir = "target/test-classes"
+grails.project.test.reports.dir = "target/test-reports"
+
+grails.project.fork = [
+    // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
+    //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+
+    // configure settings for the test-app JVM, uses the daemon by default
+    test: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
+    // configure settings for the run-app JVM
+    run: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the run-war JVM
+    war: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256, forkReserve:false],
+    // configure settings for the Console UI JVM
+    console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
+]
+
+grails.project.dependency.resolver = "maven" // or ivy
+grails.project.dependency.resolution = {
+    // inherit Grails' default dependencies
+    inherits("global") {
+        // uncomment to disable ehcache
+        // excludes 'ehcache'
+    }
+    log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
+    repositories {
+        mavenLocal()
+        mavenRepo "http://nexus.ala.org.au/content/groups/public/"
+    }
+
+    def seleniumVersion = "2.46.0"
+    def gebVersion = "0.10.0"
+
+    dependencies {
+
+        compile "org.gebish:geb-spock:${gebVersion}"
+        compile("org.seleniumhq.selenium:selenium-api:$seleniumVersion")
+        compile("org.seleniumhq.selenium:selenium-support:$seleniumVersion")
+        compile("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
+        compile("org.seleniumhq.selenium:selenium-firefox-driver:$seleniumVersion")
+    }
+
+    plugins {
+        compile ":geb:${gebVersion}"
+
+        build(":release:3.0.1",
+              ":rest-client-builder:1.0.3") {
+            export = false
+        }
+    }
+}
---
+++ src/groovy/au/org/ala/fieldcapture/FieldcaptureFunctionalTest.groovy
@@ -0,0 +1,52 @@
+package au.org.ala.fieldcapture
+
+import geb.Browser
+import geb.spock.GebReportingSpec
+import org.openqa.selenium.Cookie
+import pages.EntryPage
+import pages.LoginPage
+
+
+/**
+ * Helper class for functional tests in fieldcapture.  Contains common actions such as logging in and logging out.
+ */
+class FieldcaptureFunctionalTest extends GebReportingSpec {
+
+    /**
+     * This method will drop the ecodata-test database, then load the data stored in the
+     * test/functional/resources/${dataSetName} directory.
+     * It is intended to ensure the database is in a known state to facilitate functional testing.
+     * If the loading script fails, an Exception will be thrown.
+     *
+     * N.B. Running the script requires the current working directory to be the root of the fieldcapture-hub project.
+     * (Which should be the case unless the tests are being run via an IDE)
+     *
+     * @param dataSetName identifies the data set to load.
+     *
+     */
+    void useDataSet(String dataSetName) {
+        def dataSetPath = getClass().getResource("/resources/"+dataSetName+"/").getPath()
+
+        int exitCode = "scripts/loadFunctionalTestData.sh ${dataSetPath}".execute().waitFor()
+        if (exitCode != 0) {
+            throw new RuntimeException("Loading data set ${dataSetPath} failed.  Exit code: ${exitCode}")
+        }
+    }
+
+    def login(Browser browser, username, password) {
+
+        browser.to LoginPage, service: getConfig().baseUrl+EntryPage.url
+        browser.page.username = username
+        browser.page.password = password
+        browser.page.submit()
+
+        browser.at EntryPage
+
+        browser.driver.manage().addCookie(new Cookie("hide-intro", "1"))
+    }
+
+    def logout(Browser browser) {
+        browser.go 'https://auth.ala.org.au/cas/logout'
+
+    }
+}
